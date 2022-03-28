4eda4c91030fce05ac3c199ad594439984251a36
Druig <zz277@duke.edu>
2022-03-28 15:55:03 -0400

    update DAO layer

--- src/main/java/com/EM_System/dao/OrderMapper.java
+++ src/main/java/com/EM_System/dao/OrderMapper.java
@@ -6,11 +6,15 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface OrderMapper {
-    List<Order> getOrderList();
+    Order getMatchingSellOrder(Order order);
+
+    Order getMatchingBuyOrder(Order order);
 
     Order getOrderByID(@Param("order_id") int order_id);
 
     int createOrder(Order order);
 
     int editOrder(Order order);
+
+    int cancelOrder(@Param("order_id") int order_id);
 }
--- src/main/java/com/EM_System/dao/PositionMapper.java
+++ src/main/java/com/EM_System/dao/PositionMapper.java
@@ -3,11 +3,10 @@ package com.EM_System.dao;
 import com.EM_System.pojo.Position;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
 
 public interface PositionMapper {
 
-    Position getSpecificPosition(@Param("account_id") int account_id, String symbol_name);
+    Position getSpecificPosition(@Param("account_id") int account_id, @Param("symbol_name") String symbol_name);
 
     int createPosition(Position position);
 
