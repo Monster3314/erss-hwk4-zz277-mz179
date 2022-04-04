2876dc26a42770c611aba6bc3132d56295967074
Ming Zhong <ming.zhong@duke.edu>
2022-04-03 22:59:14 -0400

    package

--- docker-deploy/src/EM_System/src/main/java/com/EM_System/app/NIOServer.java
+++ docker-deploy/src/EM_System/src/main/java/com/EM_System/app/NIOServer.java
@@ -180,98 +180,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Task implements Runnable {
-    private Socket socket;
 
-    public Task(Socket socket) {
-        this.socket = socket;
-    }
-
-    public void run() {
-        try {
-            InputStream inputStream= null;
-						try {
-							inputStream = this.socket.getInputStream();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
-                        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
-                        String temp=null;
-                        StringBuilder sb = new StringBuilder();
-                        while(true){
-							try {
-								if ((temp = bufferedReader.readLine()) == null) break;
-                System.out.println(temp);
-                sb.append(temp);
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-							
-                        }
-             System.out.println(sb.toString());
-						XmlParser parser = null;
-						try {
-							parser = new XmlParser();
-						} catch (ParserConfigurationException | TransformerConfigurationException e) {
-							e.printStackTrace();
-						}
-						Request req = parser.parse(new ByteArrayInputStream(sb.toString().getBytes()));
-            if (req == null) {
-                System.out.println("Malformed request");
-                return;
-            }
-            ArrayList<Result> res;
-						RequestExecutor executor = null;
-						try {
-							executor = new RequestExecutor();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						res = req.exec(executor);
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-						try {
-							parser.CreateXmlResponse(baos, res);
-						} catch (TransformerException e) {
-							e.printStackTrace();
-						}
-						sb.setLength(0);
-            String ans = new String(baos.toByteArray());
-            sb.append(ans.length() + System.lineSeparator());
-            sb.append(ans);
-            String resp = sb.toString();
-						OutputStream outputStream= null;//获取一个输出流，向服务端发送信息
-						try {
-							outputStream = this.socket.getOutputStream();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						PrintWriter printWriter=new PrintWriter(outputStream);//将输出流包装成打印流
-                        printWriter.print(resp);
-                        System.out.println(resp);
-                        printWriter.flush();
-						try {
-							finalSocket.shutdownOutput();//关闭输出流
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-
-
-						//关闭相对应的资源
-						try {
-							baos.close();
-							printWriter.close();
-							outputStream.close();
-							bufferedReader.close();
-							inputStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
 
 public class NIOServer {
 
@@ -284,7 +193,7 @@ public class NIOServer {
             while(true){
                 socket = serverSocket.accept();
         				Task task = new Task(socket);
-                executer.execute(task);
+                executor.execute(task);
             }
         }catch(IOException e){
             e.printStackTrace();
